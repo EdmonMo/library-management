545e6a164834ed0b2f1efd93e5b777e309a23cd4
EdmonMo <ed.mouss4@gmail.com>
2026-05-10 21:33:41 +0300

    started working on view book page

--- app/(dashboard)/books/[id]/page.tsx
+++ app/(dashboard)/books/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { getBookById } from "@/actions/books"
 import EditBookForm from "../_components/edit-book-form"
 import { getCategoriesAction } from "@/actions/categories"
+import { auth } from "@/lib/auth"
+import ViewBook from "../_components/view-book"
 
 interface Props {
   params: Promise<{ id: string }>
@@ -15,30 +17,50 @@ export default async function BookPage({ params }: Props) {
     limit: 100,
     page: 1,
   })
+  const { user } = await auth()
 
   return (
     <>
-      <div className="mb-8">
-        <h1 className="mb-2 text-3xl font-bold text-gray-900">تعديل الكتاب</h1>
-        <p className="text-muted-foreground">
-          قم بتعديل معلومات الكتاب في مكتبتك الرقمية
-        </p>
-      </div>
-
-      <div className="mx-auto max-w-4xl space-y-6">
-        <div className="flex items-start gap-4 rounded-xl border border-blue-200 bg-linear-to-l from-blue-50 to-white p-4">
-          <div className="flex-1">
-            <h3 className="mb-1 text-lg font-bold text-gray-800">
-              تعديل كتاب في النظام
-            </h3>
-            <p className="text-sm text-gray-600">
-              يرجى ملء جميع الحقول بدقة لضمان فهرسة صحيحة للكتاب في النظام.
+      {user.role === "STUDENT" ? (
+        <>
+          <div className="mb-8">
+            <h1 className="mb-2 text-3xl font-bold text-gray-900">
+              معاينة الكتاب
+            </h1>
+            <p className="text-muted-foreground">
+              قم بمعاينة معلومات الكتاب في المكتبة الرقمية
             </p>
           </div>
-        </div>
 
-        <EditBookForm book={book} allCategories={categories} />
-      </div>
+          <ViewBook book={book} />
+        </>
+      ) : (
+        <>
+          <div className="mb-8">
+            <h1 className="mb-2 text-3xl font-bold text-gray-900">
+              تعديل الكتاب
+            </h1>
+            <p className="text-muted-foreground">
+              قم بتعديل معلومات الكتاب في مكتبتك الرقمية
+            </p>
+          </div>
+
+          <div className="mx-auto max-w-4xl space-y-6">
+            <div className="flex items-start gap-4 rounded-xl border border-blue-200 bg-linear-to-l from-blue-50 to-white p-4">
+              <div className="flex-1">
+                <h3 className="mb-1 text-lg font-bold text-gray-800">
+                  تعديل كتاب في النظام
+                </h3>
+                <p className="text-sm text-gray-600">
+                  يرجى ملء جميع الحقول بدقة لضمان فهرسة صحيحة للكتاب في النظام.
+                </p>
+              </div>
+            </div>
+
+            <EditBookForm book={book} allCategories={categories} />
+          </div>
+        </>
+      )}
     </>
   )
 }
---
+++ app/(dashboard)/books/_components/view-book.tsx
@@ -0,0 +1,5 @@
+import { BookDetailedResponse } from "@/types/types"
+
+export default function ViewBook({ book }: { book: BookDetailedResponse }) {
+  return <p>قيد التنفيذ</p>
+}
